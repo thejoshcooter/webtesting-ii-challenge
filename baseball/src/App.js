0c9c258ef3aea8c9d9a15d17e31fa74496526b8a
Josh Cooter <thejoshcooter@gmail.com>
2019-06-19 10:18:00 -0500

    wireframe Dashboard component

--- baseball/src/App.js
+++ baseball/src/App.js
@@ -1,8 +1,14 @@
 import React from 'react';
 
+// import Dashboard
+import Dashboard from './views/dashboard/Dashboard'
+
 function App() {
   return (
-    <div>hello world</div>
+    <div>
+      <h1>Web Testing 3 Challenge</h1>
+      <Dashboard />
+    </div>
   );
 }
 
---
+++ baseball/src/views/dashboard/Dashboard.js
@@ -0,0 +1,11 @@
+import React, { Component } from 'react';
+
+class Dashboard extends Component {
+    render() {
+        return (
+            <div>hello from dashboard</div>
+        );
+    }
+};
+
+export default Dashboard;
