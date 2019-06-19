c0bab89a955dc14ab536f2efe577e4ffc5b251be
Josh Cooter <thejoshcooter@gmail.com>
2019-06-19 13:48:50 -0500

    pass test - Display component renders

--- baseball/src/views/dashboard/Display.test.js
+++ baseball/src/views/dashboard/Display.test.js
@@ -1,2 +1,19 @@
 // display component import
+import React from 'react';
 import Display from './Display';
+import ReactDOM from 'react-dom';
+import { render } from '@testing-library/react';
+
+describe('<Display />', () => {
+    describe('Component Creation', () => {
+        it('renders without crashing', () => {
+           const div = document.createElement('div');
+           ReactDOM.render(<Display />, div);
+           ReactDOM.unmountComponentAtNode(div); 
+        });
+
+        it('renders without errors', () => {
+            render(<Display />)
+        });
+    });
+});
