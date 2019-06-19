d1b24f095584c8269c39d01ef2c91199e81b395e
Josh Cooter <thejoshcooter@gmail.com>
2019-06-19 10:07:06 -0500

    add app.js testing suite and todos

--- baseball/src/App.test.js
+++ baseball/src/App.test.js
@@ -1,9 +1,23 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import App from './App';
+import React from "react";
+import ReactDOM from "react-dom";
+import App from "./App";
 
-it('renders without crashing', () => {
-  const div = document.createElement('div');
-  ReactDOM.render(<App />, div);
-  ReactDOM.unmountComponentAtNode(div);
+import { render } from "@testing-library/react";
+
+describe("<App />", () => {
+  describe("Component Creation", () => {
+    it("renders without crashing", () => {
+      const div = document.createElement("div");
+      ReactDOM.render(<App />, div);
+      ReactDOM.unmountComponentAtNode(div);
+    });
+
+    it("renders without errors", () => {
+      render(<App />);
+    });
+
+  describe("Display Properties", () => {});
+    it.todo('displays the count of balls and strikes for at-bat');
+    it.todo('it updates when user records activity on dashboard');
+    });
 });
